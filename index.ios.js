b6e4105c9d333b4b4b1f3a9218e35f18fccb8da5
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-06-14 22:34:09 -0400

    able to fetch movies from rt database

--- index.ios.js
+++ index.ios.js
@@ -22,10 +22,48 @@ var MOCKED_MOVIES_DATA = [
 var movie = MOCKED_MOVIES_DATA[0];
 
 class sampleAppMovies extends Component {
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
   render() {
+    if(!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}} />
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}}/>
         <View style={styles.rightContainer}>
           <Text style={styles.title}>{movie.title}</Text>
           <Text style={styles.year}>{movie.year}</Text>
@@ -67,7 +105,7 @@ const styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
-  }
+  },
 });
 
 AppRegistry.registerComponent('sampleAppMovies', () => sampleAppMovies);
